9c06a480a54dc470eefefb1db71c6a219c7b2b8a
Wanwix <deividgt28@gmail.com>
2025-02-28 17:06:46 -0500

    Animations - Update V1
    I added her animations and started working on her movement script.

---
+++ Assets/Scripts/FayController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FayController : MonoBehaviour
+{
+    public float speed = 5f; //velocidad
+    public bool isRunning = false;
+    public bool isJumping = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("FayController script started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
